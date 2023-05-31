2eb7735d79b47a018e9b4014f5d1cdc33b892099
Ridah Behardien <RBehardien@inspiredtesting.com>
2023-05-31 15:54:31 +0200

    Added final stages

--- src/main/java/Sqlqueries/SqlQueries.java
+++ src/main/java/Sqlqueries/SqlQueries.java
@@ -86,4 +86,82 @@ public class SqlQueries {
         }
 
     }
+    public int TotalNumberOfCustomer(){
+        String SelectFromCustomers = "SELECT count(*) FROM customers";
+        int count = 0;
+
+        try (Connection conn = DriverManager.getConnection(this.url, this.user , this.password);
+             PreparedStatement ps = conn.prepareStatement(SelectFromCustomers);
+             ResultSet rs = ps.executeQuery()){
+
+            rs.next();
+            count = rs.getInt("count(*)");
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return count;
+    }
+    public int ChecksThatTheUserWithFirstNameSarahHasTwoAccounts(){
+        String SelectFromCustomers = "SELECT count(account_number) FROM customers WHERE customer_id = 1";
+        int count = 0;
+
+        try (Connection conn = DriverManager.getConnection(this.url, this.user , this.password);
+             PreparedStatement ps = conn.prepareStatement(SelectFromCustomers);
+             ResultSet rs = ps.executeQuery()){
+
+            rs.next();
+            count = rs.getInt("count(account_number)");
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return count;
+    }
+    public double totalOfAllDepositTransactions(String accountNumber){
+        String SelectFromCustomers = "Select amount FROM transactions WHERE transaction_type = 'DEPOSIT' AND account_number = " +accountNumber+ "" ;
+
+        double amount = 0 ;
+        double total = 0;
+
+        try (Connection conn = DriverManager.getConnection(this.url, this.user , this.password);
+             PreparedStatement ps = conn.prepareStatement(SelectFromCustomers);
+             ResultSet rs = ps.executeQuery()){
+
+            while (rs.next()){
+                amount  =rs.getDouble("amount");
+
+                total = total + amount;
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return total;
+    }
+    public double totalOfAllWithdrawalTransactions(String accountNumber){
+        String SelectFromCustomers = "Select amount FROM transactions WHERE transaction_type = 'WITHDRAWAL' AND account_number = " +accountNumber+ "" ;
+
+        double amount = 0;
+        double total = 0;
+
+        try (Connection conn = DriverManager.getConnection(this.url, this.user , this.password);
+             PreparedStatement ps = conn.prepareStatement(SelectFromCustomers);
+             ResultSet rs = ps.executeQuery()){
+
+            while (rs.next()){
+                amount  =rs.getDouble("amount");
+
+                total = total + amount;
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return total;
+    }
 }
+
+
--- src/test/java/sql/SqlTest.java
+++ src/test/java/sql/SqlTest.java
@@ -4,33 +4,45 @@ import Sqlqueries.SqlQueries;
 import org.example.Main;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SqlTest {
-    protected SqlQueries Sql;
 
     // Retrieve the number of customers from the database
     // Check that the total number of customers is equal to 2
     @Test
     public void getAllCustomers_checkNumber_shouldBeTwo() {
+        SqlQueries sql = new SqlQueries();
 
+        assertEquals(2,sql.TotalNumberOfCustomer());
     }
 
     // Retrieve all accounts for customer Sarah from the database
     // Check that the total number of accounts for her is 2
     @Test
     public void getAccountsForSarah_checkNumber_shouldBeTwo() {
-
+        SqlQueries sql = new SqlQueries();
+        assertEquals(2,sql.ChecksThatTheUserWithFirstNameSarahHasTwoAccounts());
     }
 
     // A test that checks that the total of all transactions is equal to 0 for a given account.
     // Doing this for a single account is enough,
     // and you’re allowed to use the account ID for that account in the query
     @Test
-    public void retrieveTransactionsForAccount_checkTotalBalance_shouldBeZero() {
+    public void retrieveTransactionsForAccount_checkTotalBalance_shouldBeZero()
+    {
+        SqlQueries sql = new SqlQueries();
+
+        String accountNumber = "83983800";
+
+        double totalDeposits = sql.totalOfAllDepositTransactions(accountNumber);
+
+        double totalWithdrawals = sql.totalOfAllWithdrawalTransactions(accountNumber);
+
+        double balance = totalDeposits - totalWithdrawals;
+
+        assertEquals(20, balance , "Balance was not 20");
 
     }
-    @Test
-    public void Queries() {
-    }
-
 }
 
